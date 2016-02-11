19dcbea5403a3281ea12ed61d79c02770c353220
Sebastian Fuentes <sebas6297@gmail.com>
2016-02-11 10:19:01 -0600

    :fire: compression and cache added

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
+var oneYear = 31557600000;
+var compress = require('compression');
+
+app.use(compress());  
+
+app.use(express.static(__dirname + '/public', { maxAge: oneYear }))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
