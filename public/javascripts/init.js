611cbf8a143ddc6c58fff5c0f20142b1b2903f35
Sebastian Fuentes <sebas6297@gmail.com>
2016-02-11 13:51:29 -0600

    :bowtie: finished first iteration(only frontend)

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -7,6 +7,7 @@ var map,
 	markersSelected=[],
 	position,
 	bounds,
+	hours = 0,
 	infowindow,
 	service,
   text = '<i class="material-icons">&#xE55C;</i>';
@@ -175,37 +176,41 @@ function randomNum(min, max){return Math.floor(Math.random() * (max - min)) + mi
 function changeRange(){
 	var range = document.getElementById('range').value;
 	document.getElementById('inner-hours').innerHTML = range+ " hours";
-	filterMarkers(range);
+	filterMarkers(range, false);
 }
-function filterMarkers(range){
+function filterMarkers(range, clear){
+	if (markers.length > 0) {
 	$(".pick-container").empty();
-	for (var i = 0; i < markers.length; i++) {
-		if (markers[i].time > range) {
-			markers[i].setVisible(false);
-		} else {
-			fillChecklist(markers[i], i);
-			markers[i].setVisible(true);
+		for (var i = 0; i < markers.length; i++) {
+			if (markers[i].time > range) {
+				markers[i].setVisible(false);
+			} else {
+				markers[i].setVisible(true);
+				if (clear) {
+					fillChecklist(markers[i], true);
+				} else {
+					fillChecklist(markers[i], false);
+				}
+			}
 		}
 	}
 }
-function fillChecklist(marker, i){
+function fillChecklist(marker, clear){
 	var text;
 	if (marker.title.length > 20 && (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || $("body").width() < 650)) {
 		marker.title = marker.title.substring(0,15) + "..."
-		console.log('test');
 	} else if (marker.title.length > 30) {
 		marker.title = marker.title.substring(0,30) + "..."
-		console.log('test');
 	}
-	if (marker.checked) {
-		text = "<div class='option container-"+i+"'><input onchange='addThisToList(this,event)' checked type='checkbox' id='"+marker.id+"' value='"+marker.time+"'><span class='title'>"+marker.title+"</span><span class='time'>Estimated time: "+marker.time+"</span></div>";
+	if (marker.checked && !clear) {
+		text = "<div class='option'><input onchange='addThisToList(this,event)' checked type='checkbox' id='"+marker.id+"' value='"+marker.time+"'><span class='title'>"+marker.title+"</span><span class='time'>Estimated time: "+marker.time+"</span></div>";
 	} else {
-		text = "<div class='option container-"+i+"'><input onchange='addThisToList(this,event)' type='checkbox' id='"+marker.id+"' value='"+marker.time+"'><span class='title'>"+marker.title+"</span><span class='time'>Estimated time: "+marker.time+"</span></div>";
+		text = "<div class='option'><input onchange='addThisToList(this,event)' type='checkbox' id='"+marker.id+"' value='"+marker.time+"'><span class='title'>"+marker.title+"</span><span class='time'>Estimated time: "+marker.time+"</span></div>";
 	}
 	$(".pick-container").append(text);
 }
 function addThisToList(el,ev){
-	var id;
+	console.log('test3');
 	var imageBlue = {
 	    url: "../images/marker-blue.svg",
 	    scaledSize: new google.maps.Size(32, 32),
@@ -218,7 +223,9 @@ function addThisToList(el,ev){
 	    origin: new google.maps.Point(0, 0),
 	    anchor: new google.maps.Point(16, 32)
 	}; 
-	id = ev.target.id;
+	var id = ev.target.id;
+	var value = parseFloat(ev.srcElement.value);
+	var range = document.getElementById('range').value;
 	if (ev.srcElement.checked) {
 		for (var i = 0; i < markers.length; i++) {
 			if (id == markers[i].id){
@@ -235,6 +242,12 @@ function addThisToList(el,ev){
 				markers[i].setIcon(image);
 			}
 		}
+		hours += value;
+		if (hours >= range) {
+			document.getElementById("result").innerHTML = "<div class='result-inner'> Please select less places or more hours</div>";
+		}else{
+			document.getElementById("result").innerHTML = "<div class='result-inner'> Total of hours: <b>"+hours+"</b></div>";
+		}
 	} else {
 		for (var i = 0; i < markers.length; i++) {
 			if (id == markers[i].id) {
@@ -243,47 +256,16 @@ function addThisToList(el,ev){
 				markersSelected.splice(i, 1);
 			}
 		}
+		hours -= value;
+		document.getElementById("result").innerHTML = "<div class='result-inner'> Total of hours: <b>"+hours+"</b></div>";
 	}
 }
-function addThisToListFromMap(marker){
-	var imageBlue = {
-	    url: "../images/marker-blue.svg",
-	    scaledSize: new google.maps.Size(32, 32),
-	    origin: new google.maps.Point(0, 0),
-	    anchor: new google.maps.Point(16, 32)
-	}; 
-	var image = {
-	    url: "../images/marker.svg",
-	    scaledSize: new google.maps.Size(32, 32),
-	    origin: new google.maps.Point(0, 0),
-	    anchor: new google.maps.Point(16, 32)
-	};
-	if (!marker.checked) {
-		for (var i = 0; i < markers.length; i++) {;
-			if (marker.id == markers[i].id){
-				markers[i].checked = true;
-				markers[i].setIcon(imageBlue);
-				markers[i].selected = true;
-				markersSelected.push(markers[i]);
-				map.panTo(markers[i].position);
-				google.maps.event.trigger(markers[i], 'click');
-				document.getElementById(marker.id).setAttribute("checked", "checked");
-				break;
-			} else if (markers[i].selected){
-				markers[i].setIcon(imageBlue);
-			} else{
-				markers[i].setIcon(image);
-			}
-		}
-	} else {
-		for (var i = 0; i < markers.length; i++) {
-			if (marker.id == markers[i].id) {
-				document.getElementById(marker.id).removeAttribute("checked");
-				markers[i].checked = false;
-				markers[i].setIcon(image);
-				markersSelected.splice(i, 1);
-			}
-		}
+function deleteAll(elem){
+	var range = document.getElementById('range').value;
+	markersSelected = [];
+	for (var i = 0; i < markers.length; i++) {
+		markers[i].checked = false;
 	}
+	filterMarkers(range, true);
 }
 google.maps.event.addDomListener(window, "load", initialize);
