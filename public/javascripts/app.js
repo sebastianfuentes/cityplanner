e2814ad2b457f0b8cd7f32479f4be60eb9cd8dc1
Sebastian Fuentes <sebas6297@gmail.com>
2016-02-11 10:09:21 -0600

    :fire: js minified

--- public/javascripts/app.js
+++
@@ -1,7 +0,0 @@
-angular.module('app', [])
-   .controller('ListControler', ['$scope', function($scope) {
-   	$scope.places = markers;
-    $scope.getPlaces = function(){
-    	$scope.places = markers;
-    };
-   }]);
